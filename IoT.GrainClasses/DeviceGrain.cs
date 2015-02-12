a459d2cf72c116b26703639495e802d62752c8c9
Andrew Hart <andrewthart098@gmail.com>
2015-02-11 22:25:29 -0500

    init commit

---
+++ IoT.GrainClasses/DeviceGrain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using IoT.GrainInterfaces;
+using Orleans;
+using Orleans.Providers;
+
+namespace IoT.GrainClasses
+{
+    public interface IDeviceGrainState : IGrainState
+    {
+        double LastValue { get; set; }
+    }
+
+    [StorageProvider(ProviderName = "AzureStore")]
+    public class DeviceGrain : Orleans.Grain<IDeviceGrainState>, IDeviceGrain
+    {
+
+        public override Task OnActivateAsync()
+        {
+            var id = this.GetPrimaryKeyLong();
+            Console.WriteLine("Activated {0}", id);
+            Console.WriteLine("Activated State: {0}", this.State.LastValue);
+            return base.OnActivateAsync();
+        }
+
+        public async Task SetTemperature(double value)
+        {
+            if (this.State.LastValue < 100 && value >= 100)
+            {
+                Console.WriteLine("High temperature recorded of {0}", value);
+            }
+            if (this.State.LastValue != value)
+            {
+                this.State.LastValue = value;
+                await this.State.WriteStateAsync();
+            }
+        }
+    }
+}
---
+++ IoT.GrainInterfaces/IDeviceGrain.cs
@@ -0,0 +1,13 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+
+namespace IoT.GrainInterfaces
+{
+    /// <summary>
+    /// Grain interface IGrain1
+    /// </summary>
+    public interface IDeviceGrain : IGrain
+    {
+        Task SetTemperature(double value);
+    }
+}
